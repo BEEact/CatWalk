03b2df489d89bf7c22965aebe2768cd444ee8d08
Evan Katz <evankatz14@gmail.com>
2021-04-18 17:24:14 -0700

    fetch hardcoded review data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import sum from '../helperFunctions/sum';
-
-// const sum = require('../helperFunctions/sum')
+import axios from 'axios';
 
 export default function App(props) {
-  let thing = sum(1,2)
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    let reviews = axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/reviews?product_id=13023', {
+      headers: {'Authorization': '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'}
+    })
+    let reviewsMeta = axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/reviews/meta?product_id=13023', {
+      headers: {'Authorization': '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'}
+    })
+
+    Promise.all([reviews, reviewsMeta])
+      .then(res => {
+        let reviewsData = res.map(item => item.data)
+        setReviews(reviewsData)
+      })
+      .catch(err => console.log('err', err));
+  }, [])
+
   return(
-    <div>Hello World {thing}</div>
+    <div>Hello World</div>
   )
 }
