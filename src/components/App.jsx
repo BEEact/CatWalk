395fc6f8f5b845beed5718e946f49f845fb51c5c
Evan Katz <evankatz14@gmail.com>
2021-04-18 20:21:10 -0700

    display review info in the ReviewSection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import sum from '../helperFunctions/sum';
 import axios from 'axios';
+import ReviewsSection from './Reviews/ReviewsSection.jsx';
 
 export default function App(props) {
   const [reviews, setReviews] = useState([]);
+  const [reviewsMeta, setReviewsMeta] = useState([]);
   useEffect(() => {
     let reviews = axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/reviews?product_id=13023', {
       headers: {'Authorization': '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'}
@@ -14,13 +16,15 @@ export default function App(props) {
 
     Promise.all([reviews, reviewsMeta])
       .then(res => {
-        let reviewsData = res.map(item => item.data)
-        setReviews(reviewsData)
+        setReviews(res[0].data)
+        setReviewsMeta(res[1].data)
       })
       .catch(err => console.log('err', err));
   }, [])
 
   return(
-    <div>Hello World</div>
+    <>
+      <ReviewsSection reviews={reviews} reviewsMeta={reviewsMeta} />
+    </>
   )
 }
